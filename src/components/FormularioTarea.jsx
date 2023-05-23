c5bf6b870ee0b4b7ff08a1a78963c7e8fbdb52e9
maxivega0 <maximilianovega600@gmail.com>
2023-05-23 18:36:16 -0300

    uso de props y funciones para actualizar el array de tareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,16 +1,35 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Necesito conservar tareas(operador spreed "...")
+    // Crea un nuevo array, toma cada uno de los elementos iterales y
+    //  copialos en un nuevo arreglo y al final agrega el valor de stateTarea(input)
+    setTareas([...tareas, tarea])
+  }
+
+
+
   return (
     <>
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="ingrese una tarea" />
-        <Button variant="primary">Agregar</Button>
-      </Form.Group>
-    </Form>
-    <ListaTareas></ListaTareas>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          {/* accediendo mediante el evento onchange a los valores del imput  onChange={(e)=> console.log(e.target.value)*/}
+          <Form.Control
+            type="text"
+            placeholder="ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">Agregar</Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
     </>
   );
 };
