d5775ed9bebc1c72213107d71f262f431e487890
maxivega0 <maximilianovega600@gmail.com>
2023-05-30 19:01:01 -0300

    formato al documento yagregado de comentaris

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,11 +1,29 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// Use effect ciclo de vida componentes
 
 const FormularioTarea = () => {
+  //* Zona de variables
+  // Creacion de la variable localStorage, que contiene nuestro array de lstaTareas
+  const tareasDelLocalStorage = JSON.parse(localStorage.getItem("listaTareas") || [])
   const [tarea, setTarea] = useState("");
   // Array de tareas
-  const [tareas, setTareas] = useState([]);
+  const [tareas, setTareas] = useState(tareasDelLocalStorage);
+
+
+  //*Zona ciclo de vida
+  // El primer parametro es una funcion anonima
+  // El nombrar "useEffect" hace que se ejecute en montaje/actualizacion
+  useEffect(()=>{
+    // Cuando el comopoennte se ha montado, actualiza el local storage
+    localStorage.setItem("listaTareas", JSON.stringify(tareas))
+    console.log("Aqui deberia guardar en local storage");
+  },[tareas])
+  // Al poner al final ",[tareas]" ignora la actualizacion del input, solo se fija en la actualizacion del array
+
+
+  //* Zona de Funciones
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,6 +41,7 @@ const FormularioTarea = () => {
     setTareas(copiaListaTareas);
   };
 
+  //* Zona componentes
   return (
     <>
       <Form onSubmit={handleSubmit}>
